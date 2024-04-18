00ee3a3abc8dab06e7c7a88c71a0606daf0ea114
Rommel Duarte Corral <enixthefenix64@gmail.com>
2024-04-18 15:54:51 -0700

    definir una nueva clase

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public string Nombre (get; set)
+		public int edad (get; set)
+		public float estatura (get; set)
+		public float peso (get; set)
+
+	}
+}
